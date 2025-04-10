2a1b3bed6dc1c5b8b2d8fc958927a76674dd1b2e
SHAIK ARSHAD <shaikarshad090909@gmail.com>
2025-04-10 22:30:55 +0530

    commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ function SearchBar({ onSearch }) {
 
   const handleSearch = async () => {
     try {
-      const API_KEY = VITE_WEATHER_API_KEY; // Replace with your OpenWeatherMap API key
+      const API_KEY = VITE_WEATHER_API_KEY; 
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`);
 
       if (response.data.coord) {
